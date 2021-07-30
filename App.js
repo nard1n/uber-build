4d6b90d7db026f462571d92bbefe7a1f1d6bd2e2
Nardin Lachowski <nardin@live.com>
2021-07-30 13:40:05 -0400

    react navigation and gesture handling imported and included the homescreen and mapscreen pages into the screen stack

--- App.js
+++ App.js
@@ -5,13 +5,37 @@ import { Provider } from "react-redux";
 import HomeScreen from './screens/HomeScreen';
 import { store } from './store';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import MapScreen from './screens/MapScreen';
 
 export default function App() {
+  const Stack = createStackNavigator();
+
   return (
     <Provider store={store}>
-      <SafeAreaProvider>
-        <HomeScreen />
-      </SafeAreaProvider>
+      <NavigationContainer>
+        <SafeAreaProvider>
+          <Stack.Navigator>
+            <Stack.Screen 
+              name='HomeScreen'
+              component={HomeScreen}
+              options={{
+                headerShown: false,
+              }}
+            />
+            <Stack.Screen 
+              name='MapScreen'
+              component={MapScreen}
+              options={{
+                headerShown: false,
+              }}
+            />
+          </Stack.Navigator>
+          {/* <HomeScreen /> */}
+        </SafeAreaProvider>
+      </NavigationContainer>
     </Provider>
   );
 }
