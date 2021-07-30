ce0f92bb1baa7b02a4cc3fa1a0c7faf1e2542fa8
Nardin Lachowski <nardin@live.com>
2021-07-30 12:34:20 -0400

    importing homescreen

--- App.js
+++ App.js
@@ -2,14 +2,13 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { Provider } from "react-redux";
+import HomeScreen from './screens/HomeScreen';
 import { store } from './store';
 
 export default function App() {
   return (
     <Provider store={store}>
-      <View style={styles.container}>
-        <Text>Let's build Uber!</Text>
-      </View>
+      <HomeScreen />
     </Provider>
   );
 }
