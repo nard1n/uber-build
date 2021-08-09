004812743411e06239381866f9a09c1fd935a112
Nardin Lachowski <nardin@live.com>
2021-08-09 15:07:19 -0400

    added the uber cars data to display with the selection logic

--- components/RideOptionsCard.js
+++ components/RideOptionsCard.js
@@ -10,10 +10,11 @@ import {
 import { Icon } from "react-native-elements";
 import tw from "tailwind-react-native-classnames";
 import { useNavigation } from "@react-navigation/native";
+import { useState } from "react";
 
 const RideOptionsCard = () => {
     const navigation = useNavigation();
-
+    const [selected, setSelected] = useState(null);
     const data = [
         {
           id: "Uber-X-123",
@@ -48,14 +49,33 @@ const RideOptionsCard = () => {
             <FlatList
                 data={data}
                 keyExtractor={(item)=> item.id}
-                renderItem = {({item}) => (
-                    <TouchableOpacity>
-                        <Text>
-                            Car
-                        </Text>
+                renderItem = {({item: {id, title, multiplier, image}, item}) => (
+                    <TouchableOpacity 
+                        onPress={() => setSelected(item)}
+                        style={tw`flex-row justify-between items-center px-10 ${id === selected?.id && 'bg-gray-200'}`}
+                    >
+                        <Image
+                            style={{
+                                width: 100,
+                                height: 100,
+                                resizeMode: "contain",
+                            }}
+                            source={{ uri: image}}
+                        />
+                        <View style={tw`-ml-6`}>
+                            <Text style={tw`text-xl font-semibold`}>{title}</Text>
+                            <Text>Travel Time</Text>
+                        </View>
+                        <Text style={tw`text-xl`}>$99</Text>
                     </TouchableOpacity>
                 )}
             />
+
+            <View>
+                <TouchableOpacity disable={!selected} style={tw`bg-black py-3 m-3 ${!selected && "bg-gray-300"}`}>
+                    <Text style={tw`text-center text-white text-xl`}>Choose {selected?.title}</Text>
+                </TouchableOpacity>
+                </View>
         </SafeAreaView>
     )
 }
