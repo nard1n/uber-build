2ab6ed3be5f01f42ea18806bc2ad4819bb95be9e
Nardin Lachowski <nardin@live.com>
2021-08-09 11:01:47 -0400

    changing nav items opacity wto be grayed out when orig nhangin t locatin is not selected

--- components/NavOptions.js
+++ components/NavOptions.js
@@ -2,7 +2,9 @@ import React, { Component } from 'react'
 import { FlatList, StyleSheet, Text, Image, TouchableOpacity, View } from 'react-native'
 import tw from 'tailwind-react-native-classnames'
 import { Icon } from "react-native-elements"
+import { useSelector } from 'react-redux'
 import { useNavigation } from '@react-navigation/native'
+import { selectOrigin } from '../slices/navSlice'
 
 const data = [
         {
@@ -21,6 +23,8 @@ const data = [
 
 const NavOptions = () => {
     const navigation = useNavigation();
+    const origin = useSelector(selectOrigin);
+
     return (
         <FlatList
             data={data}
@@ -30,8 +34,9 @@ const NavOptions = () => {
                 <TouchableOpacity
                     onPress={() => {navigation.navigate(item.screen)}}
                     style={tw`p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40`}
+                    disabled={!origin}
                 >
-                    <View>
+                    <View style={tw`${!origin && "opacity-20"}`}>
                         <Image
                             style={{ width: 120, height: 120, resizeMode: "contain" }}
                             source={{ uri: item.image }}
