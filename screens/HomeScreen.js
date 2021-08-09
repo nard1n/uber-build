380a4179b63dadad7102febd0d04863b57625afa
Nardin Lachowski <nardin@live.com>
2021-08-09 09:44:26 -0400

    adde Google maps and encorporated dispatch for updating state

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,9 +1,15 @@
 import React from 'react';
-import { StyleSheet, Text, View, SafeAreaView, Image } from 'react-native';
+import { useDispatch } from 'react-redux';
+import { StyleSheet, View, SafeAreaView, Image } from 'react-native';
 import tw from 'tailwind-react-native-classnames';
 import NavOptions from '../components/NavOptions';
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
+import { GOOGLE_MAPS_APIKEY } from "@env";
+import { setDestination, setOrigin } from '../slices/navSlice';
 
 const HomeScreen = () => {
+    const dispatch = useDispatch();
+
     return (
         <SafeAreaView style={tw`bg-white h-full p-5`}>
             <View>
@@ -14,7 +20,35 @@ const HomeScreen = () => {
                 source={{
                     uri:"https://links.papareact.com/gzs",
                 }}
-                /> 
+                />
+                <GooglePlacesAutocomplete
+                    placeholder="Where From?"
+                    styles={{
+                        container: {
+                            flex: 0,
+                        },
+                            textInput: {
+                                fontSize: 18,
+                            },
+                    }}
+                    fetchDetails={true}
+                    minLength={2}
+                    enablePoweredByContainer={false}
+                    returnKeyType={"search"}
+                    onPress={(data, details = null) => {
+                            dispatch(setOrigin({
+                                location: details.geometry.location,
+                                description: data.description,
+                            }));
+                        dispatch(setDestination(null));
+                    }}
+                    query={{
+                        key: GOOGLE_MAPS_APIKEY, //rm -rf node_modules/.cace/babel-loader/* to reload the env vars
+                        language: "en",
+                    }}
+                    nearbyPlacesAPI="GooglePlacesSearch"
+                    debounce={400}
+                />
                 <NavOptions />
             </View>
 
@@ -23,5 +57,3 @@ const HomeScreen = () => {
 };
 
 export default HomeScreen;
-
-const styles = StyleSheet.create({});
