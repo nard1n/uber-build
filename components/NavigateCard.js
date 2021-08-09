38588cab3e2859c4de5750264354551081f893c3
Nardin Lachowski <nardin@live.com>
2021-08-09 13:59:37 -0400

    adding buttons for rides and eats at the bottom of screen

--- components/NavigateCard.js
+++ components/NavigateCard.js
@@ -1,6 +1,7 @@
 import tw from 'tailwind-react-native-classnames';
 import React from 'react'
-import { StyleSheet, Text, SafeAreaView, View } from 'react-native'
+import { StyleSheet, Text, SafeAreaView, View, TouchableOpacity } from 'react-native'
+import { Icon } from "react-native-elements";
 import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
 import { GOOGLE_MAPS_APIKEY } from "@env";
 import { useDispatch } from 'react-redux';
@@ -40,6 +41,19 @@ const NavigateCard = () => {
                     />
                 </View>
                 <NavFavorites />
+            </View>
+            <View style={tw`flex-row bg-white justify-evenly py-2 mt-auto border-t border-gray-100`}> 
+                <TouchableOpacity 
+                    onPress={() => navigation.navigate("RideOptionsCard")}
+                    style={tw`flex flex-row justify-between bg-black w-24 py-3 px-4 rounded-full`}>
+                    <Icon name="car" type="font-awesome" color="white" size={16} />
+                    <Text style={tw`text-white text-center`}>Rides</Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity style={tw`flex flex-row justify-between w-24 py-3 px-4 rounded-full`}>
+                    <Icon name="fast-food-outline" type="ionicon" color="black" size={16} />
+                    <Text style={tw`text-center`}>Eats</Text>
+                </TouchableOpacity>
             </View>
         </SafeAreaView>
     )
