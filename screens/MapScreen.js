551f2bf61cae1d0172f41413893f945a963927bb
Nardin Lachowski <nardin@live.com>
2021-08-09 16:12:56 -0400

    added nav bar to screen that takesthe user back to main page

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,16 +1,24 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import tw from 'tailwind-react-native-classnames';
 import Map from '../components/Map';
 import MapView from 'react-native-maps';
 import { createStackNavigator  } from '@react-navigation/stack';
 import NavigateCard from '../components/NavigateCard';
 import RideOptionsCard from '../components/RideOptionsCard';
+import { Icon } from 'react-native-elements/dist/icons/Icon';
+import { useNavigation } from '@react-navigation/core';
 
 const MapScreen = () => {
     const Stack = createStackNavigator();
+    const navigation = useNavigation();
     return (
         <View>
+            <TouchableOpacity 
+            onPress={() => navigation.navigate('HomeScreen')}
+            style={tw`absolute top-16 left-8 bg-gray-100 z-50 p-3 rounded-full shadow-lg`}>
+                <Icon name="menu" />
+            </TouchableOpacity>
             <View style={tw`h-1/2`}>
                 <Map /> 
             </View>
