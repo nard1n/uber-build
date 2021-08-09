f5f099f05eacd4ebb985304c4d9a0e684edfe211
Nardin Lachowski <nardin@live.com>
2021-08-09 09:45:33 -0400

    added styling and pulling in the newly created map component

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,10 +1,16 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import tw from 'tailwind-react-native-classnames';
+import Map from '../components/Map';
+import MapView from 'react-native-maps';
 
 const MapScreen = () => {
     return (
         <View>
-            <Text>Map stuff...</Text>
+            <View style={tw`h-1/2`}>
+                <Map /> 
+            </View>
+            <View style={tw`h-1/2`}></View>
         </View>
     )
 }
