aeb6f1a67ba3e8a3c6e070ab44397fe192953e47
Nardin Lachowski <nardin@live.com>
2021-08-09 12:02:33 -0400

    created the navigatecard file with some styling and googleplacesautocomplete and the api key linked to get the destination details for nav directions of destination

---
+++ components/NavigateCard.js
@@ -0,0 +1,63 @@
+import tw from 'tailwind-react-native-classnames';
+import React from 'react'
+import { StyleSheet, Text, SafeAreaView, View } from 'react-native'
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
+import { GOOGLE_MAPS_APIKEY } from "@env";
+import { useDispatch } from 'react-redux';
+import { setDestination } from '../slices/navSlice';
+import { useNavigation } from '@react-navigation/core';
+
+const NavigateCard = () => {
+    const dispatch = useDispatch();
+    const navigation = useNavigation();
+
+    return (
+        <SafeAreaView style={tw`bg-white flex-1`}>
+            <Text style={tw`text-center py-5 text-xl`}>Good morning, Rider</Text>
+            <View style={tw`border-t border-gray-200 flex-shrink`}>
+                <View>
+                    <GooglePlacesAutocomplete 
+                        placeholder="Where to?"
+                        styles={toInputBoxStyles}
+                        fetchDetails={true}
+                        returnKeyType={"search"}
+                        minLength={2}
+                        onPress={(data, details = null) => {
+                            dispatch(setDestination({
+                                location: details.geometry.location,
+                                description: data.description,
+                            }));
+                            navigation.navigate('RideOptionsCard');
+                        }}
+                        enablePoweredByContainer={false}
+                        query={{
+                            key: GOOGLE_MAPS_APIKEY,
+                            language: "en",
+                        }}
+                        nearbyPlacesAPI="GooglePlacesSearch"
+                        debounce={400}
+                    />
+                </View>
+            </View>
+        </SafeAreaView>
+    )
+} 
+
+export default NavigateCard
+
+const toInputBoxStyles = StyleSheet.create({
+    container: {
+        backgroundColor: "white",
+        paddingTop: 20,
+        flex: 0,
+    },
+    textInput: {
+        backgroundColor: "#DDDDDF",
+        borderRadius: 0,
+        fontSize: 18,
+    },
+    textInputContainer: {
+        paddingHorizontal: 20,
+        paddingBottom: 0,
+    },
+});
